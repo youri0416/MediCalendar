621b168e73896683a0fdbe11f2e4d189ce84a24c
youri0416 <withuuuu0416@gmail.com>
2024-10-04 15:05:06 +0900

    P schedulesコントローラー、モデル作成

---
+++ app/models/patient_schedule.rb
@@ -0,0 +1,2 @@
+class PatientSchedule < ApplicationRecord
+end
---
+++ test/controllers/patient_schedules_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class PatientSchedulesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
