1807f539e3f71f6d97f542551f41b226e22a9940
youri0416 <withuuuu0416@gmail.com>
2024-10-04 15:06:38 +0900

    P scheduleマイグレーション

---
+++ db/migrate/20241004055139_create_patient_schedules.rb
@@ -0,0 +1,10 @@
+class CreatePatientSchedules < ActiveRecord::Migration[7.0]
+  def change
+    create_table :patient_schedules do |t|
+      t.datetime :start_at, null: false
+      t.datetime :end_at, null: false
+      t.integer :patient_type_id, null: false
+      t.timestamps
+    end
+  end
+end
