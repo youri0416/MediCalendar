893acbf7e906307de084d5f4924cea507a4fb16f
youri0416 <withuuuu0416@gmail.com>
2024-10-04 15:06:21 +0900

    Patientマイグレーション

---
+++ db/migrate/20241004055059_create_patients.rb
@@ -0,0 +1,13 @@
+class CreatePatients < ActiveRecord::Migration[7.0]
+  def change
+    create_table :patients do |t|
+      t.integer :number, null: false
+      t.string :name, null: false
+      t.integer :gender_id, null: false
+      t.date :birthday, null: false
+      t.integer :ward_id, null: false
+      t.references :doctor, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
