9d947fe3a0a03321e1e43d451d1fa7edfbdd00f3
youri0416 <withuuuu0416@gmail.com>
2024-10-05 19:15:01 +0900

    seedファイル作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+
+# Clear existing data (optional)
+Doctor.destroy_all
+Patient.destroy_all
+
+# Create fixed doctors and ensure creation with create!
+doctors = Doctor.create!([
+  { name: '山田 太郎', department_id: 1, phone_number: 1234 },
+  { name: '佐藤 花子', department_id: 2, phone_number: 2345 },
+  { name: '鈴木 一郎', department_id: 1, phone_number: 3456 },
+  { name: '田中 次郎', department_id: 3, phone_number: 4567 }
+])
+
+# Confirm that doctors array is correctly populated
+
+
+# Create fixed patients and ensure creation with create!
+patients = Patient.create!([
+  { number: 1, name: '高橋 健', gender_id: 1, birthday: '1985-05-15', ward_id: 1, doctor_id: doctors[0].id },
+  { number: 2, name: '田村 美咲', gender_id: 2, birthday: '1990-08-20', ward_id: 2, doctor_id: doctors[1].id },
+  { number: 3, name: '山本 修', gender_id: 1, birthday: '1978-12-30', ward_id: 1, doctor_id: doctors[2].id },
+  { number: 4, name: '中村 あかり', gender_id: 2, birthday: '2000-03-10', ward_id: 3, doctor_id: doctors[3].id }
+])
+
+# Confirm that patients were created
+
