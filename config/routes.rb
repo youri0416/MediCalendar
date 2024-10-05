4c154607a2a03095dcbad9c03160f7d9d5254edd
youri0416 <withuuuu0416@gmail.com>
2024-10-05 19:16:41 +0900

    ルーティング再度設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
+  root to: 'home#index' 
+  resources :doctors do
+    resources :doctor_schedules
+  end
 
+  resources :patients do
+    resources :patient_schedules
+  end
+  
 end
