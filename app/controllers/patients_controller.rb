8f82bb68bfcb86b2f3a5cd0d27d39b760c1729c5
youri0416 <withuuuu0416@gmail.com>
2024-10-05 21:18:23 +0900

    コントローラー修正

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -4,7 +4,7 @@ class PatientsController < ApplicationController
   end
 
   def show
-    @patient = Patient.find(params[:patient_id]) # 患者を取得
+    @patient = Patient.find(params[:id]) # 患者を取得
     @patient_schedules = @patient.patient_schedules # 患者のスケジュールを取得
     @schedules = @patient_schedules.map do |schedule| 
       { title: schedule.title, start: schedule.start_at, end: schedule.end_at } 
