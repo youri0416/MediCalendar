2b5c14d52f403e5c2ddcf19c827b4f7473e0500d
youri0416 <withuuuu0416@gmail.com>
2024-10-05 21:16:39 +0900

    homeコントローラー削除

--- app/controllers/home_controller.rb
+++
@@ -1,8 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    # @patient = Patient.find(params[:patient_id]) # 患者を取得
-    # @patient_schedules = @patient.patient_schedules # 患者のスケジュールを取得
-    # @schedules = @patient_schedules.map do |schedule| 
-    #   { title: schedule.title, start: schedule.start_at, end: schedule.end_at } 
-  end
-end
