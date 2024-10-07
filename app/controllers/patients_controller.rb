f9fec4e6ba2adbbbc6826e74cae2b4fac7c128fa
youri0416 <withuuuu0416@gmail.com>
2024-10-07 15:42:17 +0900

    patientsコントローラー修正
    患者カレンダー表示

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -7,7 +7,9 @@ class PatientsController < ApplicationController
     @patient = Patient.find(params[:id]) # 患者を取得
     @patient_schedules = @patient.patient_schedules # 患者のスケジュールを取得
     @schedules = @patient_schedules.map do |schedule| 
-      { title: schedule.title, start: schedule.start_at, end: schedule.end_at } 
+      { title: "#{schedule.patient_type_id}", start: schedule.start_at, end: schedule.end_at } 
     end
   end
+
+  
 end
