a767431325d8c3afd328c01b599ede2b81651bb8
youri0416 <withuuuu0416@gmail.com>
2024-10-09 16:27:58 +0900

    アソシエーション

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,3 +1,5 @@
 class Doctor < ApplicationRecord
   has_many :patients
+  has_many :doctor_schedules
+  has_many :tasks
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
+  belongs_to :patient
   belongs_to :doctor
 end
