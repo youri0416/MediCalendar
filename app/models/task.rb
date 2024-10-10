818e68b154a228f271cf6e1953a127384d15425d
youri0416 <withuuuu0416@gmail.com>
2024-10-10 18:22:14 +0900

    taskで使用するアクティブハッシュ作成

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,10 @@
 class Task < ApplicationRecord
   belongs_to :doctor
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :title
+  belongs_to :occupation
+
+  validates :title_id, numericality: { other_than: 1 } 
+  validates :occupation_id, numericality: { other_than: 1 } 
+ 
 end
