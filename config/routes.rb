b5d4a3714e15261f6cc67befcd0744dc9087e571
youri0416 <withuuuu0416@gmail.com>
2024-10-10 14:09:13 +0900

    ルーティング設定し直し

--- config/routes.rb
+++ config/routes.rb
@@ -8,18 +8,3 @@ Rails.application.routes.draw do
     end
   end
 end
-
-
-# Rails.application.routes.draw do
-#   root to: 'patients#index' 
-#   resources :doctors do
-#     resources :doctor_schedules
-#     resources :tasks
-#   end
-
-#   resources :patients do
-#     resources :patient_schedules
-#     # resources :tasks
-#   end
-  
-# end
