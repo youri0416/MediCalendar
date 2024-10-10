31183fbbbd881a95a723e766ff34f9a5d86f9298
youri0416 <withuuuu0416@gmail.com>
2024-10-10 14:05:33 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,24 @@ Rails.application.routes.draw do
   root to: 'patients#index' 
   resources :doctors do
     resources :doctor_schedules
-    resources :tasks
+    resources :patients do
+      resources :tasks
+      resources :patient_schedules
+    end
   end
-
-  resources :patients do
-    resources :patient_schedules
-  end
-  
 end
+
+
+# Rails.application.routes.draw do
+#   root to: 'patients#index' 
+#   resources :doctors do
+#     resources :doctor_schedules
+#     resources :tasks
+#   end
+
+#   resources :patients do
+#     resources :patient_schedules
+#     # resources :tasks
+#   end
+  
+# end
