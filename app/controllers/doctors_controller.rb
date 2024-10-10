c267786f0e0db173d5c1fe9484a84c7756c9305d
youri0416 <withuuuu0416@gmail.com>
2024-10-10 16:08:32 +0900

    doctorコントローラー設定

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,2 +1,10 @@
 class DoctorsController < ApplicationController
+  def index
+    @doctors = Doctor.all
+  end
+  
+  def show
+    @doctor = Doctor.find(params[:id]) # 医者を取得
+    @patients = @doctor.patients # この医者に関連する患者を取得
+  end
 end
