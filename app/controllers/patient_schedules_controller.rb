6c88218f6134b30d95df6f8a354e1bb61dd0b696
youri0416 <withuuuu0416@gmail.com>
2024-10-11 16:05:15 +0900

    患者カレンダーに予定を登録、表示

--- app/controllers/patient_schedules_controller.rb
+++ app/controllers/patient_schedules_controller.rb
@@ -2,12 +2,13 @@ class PatientSchedulesController < ApplicationController
   def new
     @patient_schedule = PatientSchedule.new
     @selected_date = params[:date] # 選択した日付を渡す
+    @patient_types = PatientType.all
   end
 
   def create
     @patient_schedule = PatientSchedule.new(patient_schedule_params)
     if @patient_schedule.save
-      redirect_to patient_path(@patient_schedule.patient_id)
+      redirect_to doctor_patient_path(@patient_schedule.patient.doctor_id, @patient_schedule.patient_id)
     else
       render :new
     end
--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -8,7 +8,7 @@ class PatientsController < ApplicationController
     @patient = @doctor.patients.find(params[:id]) # 患者を取得
     @patient_schedules = @patient.patient_schedules # 患者のスケジュールを取得
     @schedules = @patient_schedules.map do |schedule| 
-      { title: "#{schedule.patient_type_id}", start: schedule.start_at, end: schedule.end_at } 
+      { title: "#{schedule.patient_type.name}", start: schedule.start_at, end: schedule.end_at } 
     end
   end
 
