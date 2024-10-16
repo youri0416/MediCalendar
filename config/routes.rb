cb3b549ce12806917be4162d174469a578f684ee
youri0416 <withuuuu0416@gmail.com>
2024-10-16 18:32:08 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  root to: 'patients#index' 
+  root to: 'patients#index'
+
   resources :doctors do
     resources :doctor_schedules
     resources :patients do
-      resources :tasks
+      resources :tasks do
+        member do
+          patch 'toggle_complete' # toggle_complete アクションのルートを追加
+        end
+      end
       resources :patient_schedules
     end
   end
