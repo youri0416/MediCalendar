9daf4374d647b31f363da08f99ee446766a0f02a
youri0416 <withuuuu0416@gmail.com>
2025-04-16 10:02:35 +0900

    Add ON DELETE CASCADE to doctor_schedules

---
+++ db/migrate/20250416010134_add_on_delete_cascade_to_doctor_schedules.rb
@@ -0,0 +1,6 @@
+class AddOnDeleteCascadeToDoctorSchedules < ActiveRecord::Migration[7.0]
+  def change
+    remove_foreign_key :doctor_schedules, :doctors
+    add_foreign_key :doctor_schedules, :doctors, on_delete: :cascade
+  end
+end
